c7c151e12d6be621fb69ac8c1aa65dbe96c98de8
Abdurrahman Abdulaziz <abdurrahmanabdulaziz1515@gmail.com>
2022-02-04 10:58:41 +0100

    initial styling setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,27 @@
 <script setup lang="ts">
+import { darkTheme, NCard, NConfigProvider } from 'naive-ui';
 </script>
 
 <template>
-  <router-view></router-view>
+  <NConfigProvider :theme="darkTheme">
+    <NCard class="container">
+      <router-view></router-view>
+    </NCard>
+  </NConfigProvider>
 </template>
 
+<style scoped>
+.container {
+  height: 100vh;
+  width: 100vw;
+  overflow: auto;
+  border-radius: 0;
+}
+</style>
+
+<style>
+body,
+html {
+  margin: 0;
+}
+</style>
